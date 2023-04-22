8768b62eac71371c291eb2a5ab053e3d4fe2d34b
willem <1121567132@qq.com>
2023-04-22 08:36:45 +0800

    错误信息重定向

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Applicaion error: {e}");
+        eprintln!("Applicaion error: {e}");
         process::exit(1);
     };
 }
